55b96fe937eed6d31f9afaaf2557b5d0b982ae09
Dom58 <dndahimana58@gmail.com>
2022-11-07 18:32:27 +0200

    fixing bugs final

--- text-to-speech/main.js
+++ text-to-speech/main.js
@@ -8,48 +8,48 @@ const stopButton = document.getElementById("stop-button");
 const textInput = document.getElementById("text");
 const speedInput = document.getElementById("speed");
 
-let currentCharacter; 
+let currentChar;
 
-playButton.addEventListener("click", () => { 
+playButton.addEventListener("click", () => {
     playText(textInput.value); 
 });
 
 pauseButton.addEventListener("click", pauseText);
 stopButton.addEventListener("click", stopText);
 
-speedInput.addEventListener("input", () => { 
+speedInput.addEventListener("input", () => {
     stopText();
-    playText(utterance.text.substring(currentCharacter));
+    playText(utterance.text.substring(currentChar));
 });
 
-const utterance = new SpeechSynthesisUtterance(); 
+const utterance = new SpeechSynthesisUtterance();
 
 utterance.addEventListener("end", () => {
     textInput.disabled = false;
-}); 
+});
 
 utterance.addEventListener("boundary", (e) => {
-    currentCharacter = e.charIndex;
-}); 
+    currentChar = e.charIndex;
+});
 
-function playText(text) { 
+function playText(text) {
     if(speechSynthesis.paused && speechSynthesis.speaking) {
         return speechSynthesis.resume();
-    } 
+    }
     
     if(speechSynthesis.speaking) return;
 
-    utterance.text = text; 
-    utterance.rate = speedInput.value || 1; 
+    utterance.text = text;
+    utterance.rate = speedInput.value || 1;
     textInput.disabled = true;
-    speechSynthesis.speak(utterance); 
-} 
+    speechSynthesis.speak(utterance);
+}
 
-function pauseText() { 
+function pauseText() {
     if(speechSynthesis.speaking) speechSynthesis.pause();
-} 
-    
+}
+
 function stopText() {
-    speechSynthesis.resume(); 
-    speechSynthesis.cancel(); 
+    speechSynthesis.resume();
+    speechSynthesis.cancel();
 }
